e43bd6401985aedba934952cd28efb205d3e0260
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-08 14:26:34 +0800

    fix

--- my-library/routes/web.php
+++ my-library/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('books', BooksController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
